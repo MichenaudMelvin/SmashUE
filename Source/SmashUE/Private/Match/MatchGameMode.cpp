ae071d9ce91e52338da3b5e4400567e5e8b41570
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2024-11-04 16:27:52 +0100

    find players start functions

--- Source/SmashUE/Private/Match/MatchGameMode.cpp
+++ Source/SmashUE/Private/Match/MatchGameMode.cpp
@@ -2,3 +2,31 @@
 
 
 #include "Match/MatchGameMode.h"
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void AMatchGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AArenaPlayerStart*> PlayerStartsPoints;
+	FindPlayerStartActorsInArena(PlayerStartsPoints);
+}
+
+void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultActors)
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(this, AArenaPlayerStart::StaticClass(), FoundActors);
+
+	for (AActor* FoundActor : FoundActors)
+	{
+		AArenaPlayerStart* ArenaPlayerStartActor = Cast<AArenaPlayerStart>(FoundActor);
+		if(ArenaPlayerStartActor == nullptr)
+		{
+			continue;
+		}
+
+		ResultActors.Add(ArenaPlayerStartActor);
+	}
+}
--- Source/SmashUE/Public/Match/MatchGameMode.h
+++ Source/SmashUE/Public/Match/MatchGameMode.h
@@ -6,8 +6,16 @@
 #include "GameFramework/GameModeBase.h"
 #include "MatchGameMode.generated.h"
 
+class AArenaPlayerStart;
+
 UCLASS()
 class SMASHUE_API AMatchGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+private:
+	void FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultActors);
 };
